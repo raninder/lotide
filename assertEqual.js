b4daedfee91287ec32b7d360e60d209834f72c61
raninder <raninderdhillon@gmail.com>
2021-10-26 02:29:04 +0000

    assert

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+if(actual === expected){
+    console.log("\uD83D\uDE00"+"Assertion Passed: " +actual+ " === "+expected);
+}
+else {
+    console.log("Assertion Failed: " +actual+ " !== "+expected);
+}
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Labs", "Labs");
+assertEqual(1, 10);
+//Assertion Failed: Lighthouse Labs !== Bootcamp
+//Assertion Passed: 1 === 1
