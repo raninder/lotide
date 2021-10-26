1de0764a8da7e34b2d2950d9c668c2083654bef0
raninder <raninderdhillon@gmail.com>
2021-10-26 03:00:13 +0000

    tail

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+if(actual === expected){
+    console.log("\uD83D\uDE00"+"Assertion Passed: " +`${actual}`+ " === "+`${expected}`);
+}
+else {
+    console.log("Assertion Failed: " +`${actual}`+ " !== "+`${expected}`);
+}
+};
+const tail = function(arr){
+    let ele = arr.slice(1,arr.length);
+    return ele;
+}
+
+// TEST CODE
+console.log(tail([3,4,5]));
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); 
+/*assertEqual(head([5,6,7]), 5);
+assertEqual(head([5]), 5);
+assertEqual(head([]), 5);*/
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//Assertion Failed: Lighthouse Labs !== Bootcamp
+//Assertion Passed: 1 === 1
