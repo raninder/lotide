b0e0364d0da0fd1559afaf7d45e65a4083878cb7
raninder <raninderdhillon@gmail.com>
2021-10-26 02:50:57 +0000

    head

---
+++ head.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+if(actual === expected){
+    console.log("\uD83D\uDE00"+"Assertion Passed: " +`${actual}`+ " === "+`${expected}`);
+}
+else {
+    console.log("Assertion Failed: " +`${actual}`+ " !== "+`${expected}`);
+}
+};
+const head = function(arr){
+    let ele = arr.shift();
+    return ele;
+}
+
+// TEST CODE
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head([5]), 5);
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//Assertion Failed: Lighthouse Labs !== Bootcamp
+//Assertion Passed: 1 === 1
