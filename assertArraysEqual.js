fa227301b28da0e503a012d13e7fe1ec30561c49
raninder <raninderdhillon@gmail.com>
2021-11-02 16:49:04 +0000

    updated lotide

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -12,7 +12,7 @@ function assertArraysEqual( arr1, arr2) {
     }
 }    
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, "1"]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [3, 2, "1"]);
 
 module.exports = assertArraysEqual;
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,21 @@
 
-const tail = require("../tail.js");
-const assertEqual = require("../assertEqual.js");
+const assert = require('chai').assert;
+const tail  = require('../tail');
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); 
+describe("#tail", () => {
+  it("returns [2,3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+  });
+  it("returns '[]' for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });
+  it("returns []] for []", () => {
+    assert.deepEqual(tail([]), []); 
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']); 
+  });
+});
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]); 
